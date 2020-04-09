e81687c24b1f454838c7fd600b7cdf72d3ab1263
itskamranweb <ahmedkamran265@gmail.com>
2020-04-10 00:31:27 +0600

    oddk

--- resources/pages/blog/index.vue
+++ resources/pages/blog/index.vue
@@ -5,13 +5,13 @@
                 data-ad-client="ca-app-pub-5005018703171891"
                 data-ad-slot="1234567890">
             </Adsense> -->
-            
+<!--             
 <script>
   (adsbygoogle = window.adsbygoogle || []).push({
     google_ad_client: "ca-pub-5005018703171891",
     enable_page_level_ads: true
   });
-</script>
+</script> -->
 
     </div>
 </template>
