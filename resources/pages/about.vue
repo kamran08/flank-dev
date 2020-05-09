fe2a6eb86413d765934731b37ede41333b296ff8
itskamranweb <ahmedkamran265@gmail.com>
2020-05-09 06:51:33 +0600

    up about

--- resources/pages/about.vue
+++ resources/pages/about.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="pt-120">
+      
         
         <!-- Header-->
         
@@ -9,7 +10,10 @@
             <div class="container">
                 <div class="section-content">
                     <div class="row">
-                        <div class="col-md-3">
+                           <div class="col-md-3 col-sm-3">
+                            <!-- <div class="abp-list-box"> -->
+
+                        <!-- <div class="col-md-3"> -->
                             <h3>About Flank</h3>
                             <ul class="abp-list-box-link">
                                 <li class="active"><nuxt-link to="/about">About Us</nuxt-link></li>
@@ -20,8 +24,61 @@
                                 <li><nuxt-link to="/account_closure">Close Your Account</nuxt-link></li>
                             </ul>
                         </div>
-                        <div class="col-md-9">
-                            
+                           <!-- </div> -->
+                         <div class="col-md-9 col-sm-9">
+                             <h2 class="fl-abs">About Us</h2>
+
+                             <div>
+                                <div class="new-about-header mt-30">
+                                    <p class="mt-20"> Go Flank is the largest online destination for sports coach ratings from the youth to professional level. .</p>
+                                </div>
+                               
+                            </div>
+                             <div>
+                                <div class="new-about-header mt-30">
+                                     <p class="mt-20"> 
+                                          Users from all over the world can submit a rating, leave review, share their personal story and experience of current and past coaches.  
+                                    </p>
+                                </div>
+                               
+                            </div>
+                             <div>
+                                <div class="new-about-header mt-30">
+                                     <p class="mt-20"> 
+                                         The mission of Go Flank is to give a voice to parents, players and coaches.
+                                    </p>
+                                </div>
+                               
+                            </div>
+                             <div>
+                                <div class="new-about-header mt-30">
+                                     <p class="mt-20"> 
+                                         Go Flanks leverages user-generated content, as well as cross-promotions with other sports platforms to make <nuxt-link to="/">Goflank.com</nuxt-link> one of the the highest trafficked websites for quickly researching and rating coaches, teams, and schools around the world.  
+                                    </p>
+                                </div>
+                               
+                            </div>
+                             <div>
+                                <div class="new-about-header mt-30">
+                                     <p class="mt-20"> 
+                                         Players, parents, & coaches can comment on and create new coach profiles by searching for a coach or school name on our 
+                                        <nuxt-link to="/write_review">Write a Review Page.</nuxt-link>
+                                         
+                                    </p>
+                                </div>
+                               
+                            </div>
+
+                           <br>
+                           <br>
+                           <br>
+                           <br>
+                           <br>
+                           <br>
+                           <p> 
+                                 
+                                 .
+                           </p>
                         </div>
                     </div>
                 </div>
